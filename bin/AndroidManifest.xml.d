2f342c136f21c858196bcc58b57492a84976523d
Forest Ka <forestcgk@gmail.com>
2013-08-08 21:38:16 -0700

    Basic framework

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/forest/projects/voicelink/bin/AndroidManifest.xml : \
+/home/forest/projects/voicelink/AndroidManifest.xml \
---
+++ gen/R.java.d
@@ -0,0 +1,8 @@
+/home/forest/projects/voicelink/gen/co/voicelink/R.java \
+ : /home/forest/projects/voicelink/res/values/strings.xml \
+/home/forest/projects/voicelink/res/layout/main.xml \
+/home/forest/projects/voicelink/res/drawable-xhdpi/ic_launcher.png \
+/home/forest/projects/voicelink/res/drawable-hdpi/ic_launcher.png \
+/home/forest/projects/voicelink/res/drawable-mdpi/ic_launcher.png \
+/home/forest/projects/voicelink/res/drawable-ldpi/ic_launcher.png \
+/home/forest/projects/voicelink/bin/AndroidManifest.xml \
